29adbb18238725c363a58f535e0784efd340a858
ayeshaa24 <ayeshakhtar24@gmail.com>
2020-02-02 14:58:59 +0000

    Started sponsors page

--- src/Home.js
+++ src/Home.js
@@ -34,8 +34,16 @@ const divPurple = {
 }
 
 const aboutUs = (
-        <p>UniCS is the University of Manchester's tech society. We are a big community of people with a passion for technology and creating innovative solutions for everyone to enjoy. Through our hackathons and other events, we aim to bring students and industry closer together. When we aren’t organising our own events we also travel to events in places like Barcelona, Helsinki, and Bordeaux, amongst other exotic destinations, often free due to various hackathon sponsors. Come join us to see what we get up to this year!
-        <br/>Our own events are suited to every level of knowledge and all degree courses. If you are someone who is interested in knowing how they can use computers to see an idea come to life, UniCS is the society for you. It doesn’t matter if you have never touched a computer or been programming for years, you will never be bored at our events. And if you still aren’t convinced, we have free pizza (and occasionally drinks). </p>
+        <p>UniCS is the University of Manchester's tech society. We are a big community of people with 
+        a passion for technology and creating innovative solutions for everyone to enjoy. Through our 
+        hackathons and other events, we aim to bring students and industry closer together. When we aren’t 
+        organising our own events we also travel to events in places like Barcelona, Helsinki, and Bordeaux, 
+        amongst other exotic destinations, often free due to various hackathon sponsors. Come join us to see 
+        what we get up to this year!<br/><br/>Our own events are suited to every level of knowledge and all degree
+         courses. If you are someone who is interested in knowing how they can use computers to see an idea 
+         come to life, UniCS is the society for you. It doesn’t matter if you have never touched a computer 
+         or been programming for years, you will never be bored at our events. And if you still aren’t convinced, 
+         we have free pizza (and occasionally drinks). </p>
 )
 export default function Home() {
     return(
--- src/Sponsors.js
+++ src/Sponsors.js
@@ -1,8 +1,75 @@
-import React from "react"
+import React from "react";
+import bet365_logo from './assets/bet365_logo.png';
+import gt_logo from './assets/gt_logo.png'
+//find non-copyright??
+import './font.css';
+import './sponsors.css';
+import styled from 'styled-components';
+
+const Logo = styled.div`
+    background-color: #d3d3d3;
+    padding-top: 5%;
+    padding-bottom: 5%;
+    height: 200px;
+    margin:auto;
+    position: relative;
+`;
+
+
+const bet_text = (
+    <p>bet365, the world's leading online betting and gaming company, is a driving force 
+    in the development of enterprise and Internet technology. Delivering an unrivalled 
+    online experience to more than 45 million customers in 20 languages. Innovation 
+    continues to be our lifeblood and we pride ourselves on the standards of customer 
+    service we deliver. Driven by a shared vision to be the best in our industry, a 
+    technology team in excess of 1,300 people work in house to provide the technological 
+    advances that enable us to maintain our leadership position. Giving our teams the freedom 
+    to innovate is essential to our success. Our award winning business has worked hard to 
+    cultivate a culture of creativity where good ideas and a techno entrepreneurial attitude 
+    are encouraged and rewarded. Today we continue to push technological boundaries and break 
+    new ground in software innovation. </p>
+)
+
+const gt_text = (
+    <p>We are GradTouch. The innovative job advertising platform for graduate employers of all 
+    sizes. Our employer profiles showcase your company ethos and culture, giving your employer 
+    brand the platform it needs to attract the best and brightest in graduate talent. We reach 
+    75% of the graduate market every week across our combined digital platforms. Each month, we 
+    reach 3 million 18-24 year-olds across Facebook, Instagram and Twitter, making us the UK’s 
+    largest graduate social network. 
+    <a target="_blank" href="https://www.gradtouch.com/">gradtouch.com</a>.</p>
+)
 export default function Sponsors() {
     return(
         <div>
-            <h1>Sponsors Page</h1>
+            <h1>Our Sponsors</h1>
+            <h2>Gold</h2>
+            
+            <figure>
+                <Logo>
+                    <a target="_blank" href="https://www.bet365.com/#/HO/">
+                        <img src={bet365_logo}
+                                alt="" style={{height: '100%'}} />
+                    </a>
+                </Logo>
+                <figcaption>
+                    {bet_text}
+                </figcaption>
+            </figure>
+
+            <h2>Silver</h2>
+            <figure>
+                <Logo>
+                    <a target="_blank" href="https://www.gradtouch.com/register?id=TECH-SOCIETY&src=">
+                        <img src={gt_logo}
+                                alt="" style={{width: '90%'}} />
+                    </a>
+                </Logo>
+                <figcaption>
+                    {gt_text}
+                </figcaption>
+            </figure>
         </div>
+        
     )
 }

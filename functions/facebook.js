c2bd9c142ca6390d94c641904e409f4ba01a7722
Sean Parker <sean.parker067@gmail.com>
2020-03-08 18:33:53 +0000

    Add facebook events lambda function

---
+++ functions/facebook.js
@@ -0,0 +1,30 @@
+// Need to use default import, per https://github.com/netlify/netlify-lambda#debugging
+const fetch = require('node-fetch').default;
+
+const API_ENDPOINT =
+  'https://graph.facebook.com/v4.0/unicsmanchester/events?fields=name%2Cstart_time%2Cend_time%2Cplace';
+
+// eslint-disable-next-line no-unused-vars
+exports.handler = async (event, _context) => {
+  let response;
+  const url = `${API_ENDPOINT}&access_token=${process.env.FB_ACCESS_TOKEN}`;
+  try {
+    response = await fetch(url, { method: 'GET' });
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message,
+      }),
+    };
+  }
+
+  let results = await response.json();
+  return {
+    statusCode: results.error ? 400 : 200,
+    body: JSON.stringify({
+      data: results.data,
+    }),
+  };
+};

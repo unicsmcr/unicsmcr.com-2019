25530a1e8eb5150871bf354a1856c3da9cf6f317
Ayesha <ayeshakhtar24@gmail.com>
2020-03-23 15:05:06 +0000

    added basic slideshow

--- src/home/Home.js
+++ src/home/Home.js
@@ -4,6 +4,10 @@ import '../font.css';
 import './Home.css';
 import aboutus from './aboutus/aboutus';
 import user from '../assets/user.png';
+// example pics
+import guh_2019 from '../assets/guh_2019.jpg';
+import sh_2019 from '../assets/sh_2019.jpg';
+import guh_2018 from '../assets/guh_2018.jpg';
 
 const Header = styled.div`
   color: white;
@@ -66,6 +70,49 @@ export default class Home extends React.Component {
           >
             Computer Science Society
           </h2>
+        </div>
+
+        <div className="section-black">
+          <div className="slideshow-container p-md-4 col-12 col-md-8">
+            <div id="slideshow" className="carousel slide" data-ride="carousel">
+              <ol className="carousel-indicators">
+                <li data-target="#slideshow" data-slide-to="0" className="active"></li>
+                <li data-target="#slideshow" data-slide-to="1"></li>
+                <li data-target="#slideshow" data-slide-to="2"></li>
+              </ol>
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img className="d-block w-100" src={guh_2019} alt="First slide"/>
+                  <div className="carousel-caption d-none d-md-block">
+                    <h5>Event 1</h5>
+                    <p>Caption 1</p>
+                  </div>
+                </div>
+                <div className="carousel-item">
+                  <img className="d-block w-100" src={sh_2019} alt="Second slide"/>
+                  <div className="carousel-caption d-none d-md-block">
+                    <h5>Event 2</h5>
+                    <p>Caption 2</p>
+                  </div>
+                </div>
+                <div className="carousel-item">
+                  <img className="d-block w-100" src={guh_2018} alt="Third slide"/>
+                  <div className="carousel-caption d-none d-md-block">
+                    <h5>Event 3</h5>
+                    <p>Caption 3</p>
+                  </div>
+                </div>
+              </div>
+              <a className="carousel-control-prev" href="#slideshow" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
+              </a>
+              <a className="carousel-control-next" href="#slideshow" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+              </a>
+            </div>
+          </div>
         </div>
 
         <div className="section-purple">
